167e89570f08a6ccd1079edae39aac91a28c550d
Sukeerth <ssvegaraju@gmail.com>
2020-10-02 18:19:58 -0700

    Base movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float jumpHeight = 15f;
+    public float rotateSpeed = 120f;
+    public float decelerationAmount = 2f;
+
+    private Rigidbody rigid;
+    private float yVel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+
+        rigid.velocity += input.normalized;
+        yVel = rigid.velocity.y;
+        yVel += Physics.gravity.y * Time.deltaTime;
+        rigid.velocity = Vector3.Lerp(rigid.velocity, Vector3.up * yVel, decelerationAmount * Time.deltaTime);
+    }
+
+    void Jump() {
+        rigid.AddForce(Vector3.up * jumpHeight);
+    }
+}
--- Assets/Scripts/TornadoObject.cs
+++ Assets/Scripts/TornadoObject.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class TornadoObject : MonoBehaviour
 {
     public bool isStuck = false;
-    public float rotateSpeed = 15f;
+    public Vector2 rotateSpeed = new Vector2(25, 50);
 
     // Update is called once per frame
     void Update()
     {
         if (isStuck)
             return;
-        transform.RotateAround(Vector3.up, new Vector3(0, transform.position.y, 0), rotateSpeed * Time.deltaTime);
+        float speed = Random.Range(rotateSpeed.x, rotateSpeed.y);
+        transform.RotateAround(Vector3.up, new Vector3(0, transform.position.y, 0), speed * Time.deltaTime);
         transform.Rotate(transform.forward);
     }
 }
